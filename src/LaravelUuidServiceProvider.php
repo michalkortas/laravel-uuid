74f73e785a3e6d462b0512a41bbc764dca6af7d9
Michal Kortas <michal@webroad.pl>
2021-02-19 09:20:06 +0100

    Create Service Privider

---
+++ src/LaravelUuidServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace michalkortas\LaravelUuid;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelUuidServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        //
+    }
+}
